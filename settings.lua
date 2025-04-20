695a990b1c0faa27fa1fb63f40a7e16a24830957
Shinythunder <130332657+Shinythunder@users.noreply.github.com>
2025-04-20 05:14:30 -0400

    Update settings.lua

--- settings.lua
+++ settings.lua
@@ -8,15 +8,3 @@ return function(Rayfield, Window)
         end,
     })
 
-    tab:CreateInput({
-        Name = "Webhook URL",
-        CurrentValue = "",
-        PlaceholderText = "Enter your Webhook URL",
-        RemoveTextAfterFocusLost = false,
-        Flag = "WebhookInput",
-        Callback = function(Text)
-            _G.WebhookURL = Text  -- Store globally so other files can access
-            print("Webhook URL set to: " .. _G.WebhookURL)
-        end,
-    })
-end
