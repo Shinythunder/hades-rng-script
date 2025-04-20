28e766f98726e0c80fc02e5dbef4d0b4be434737
Shinythunder <130332657+Shinythunder@users.noreply.github.com>
2025-04-20 00:23:23 -0400

    Update settings.lua

--- settings.lua
+++ settings.lua
@@ -1,1 +1,10 @@
+return function(Rayfield, Window)
+    local tab = Window:CreateTab("Settings", 4483362458)
 
+    tab:CreateButton({
+        Name = "Destroy GUI",
+        Callback = function()
+            Rayfield:Destroy()
+        end,
+    })
+end
