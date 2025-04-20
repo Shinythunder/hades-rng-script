59825ce08a0c3a7b360c9a85c10a614f13f4f17a
Shinythunder <130332657+Shinythunder@users.noreply.github.com>
2025-04-20 04:53:54 -0400

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -25,4 +25,6 @@ local Window = Rayfield:CreateWindow({
 -- Load each category script
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Shinythunder/hades-rng-script/refs/heads/main/items.lua"))()(Rayfield, Window)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Shinythunder/hades-rng-script/refs/heads/main/teleports.lua"))()(Rayfield, Window)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Shinythunder/hades-rng-script/refs/heads/main/alerts.lua"))()(Rayfield, Window)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Shinythunder/hades-rng-script/refs/heads/main/settings.lua"))()(Rayfield, Window)
+
