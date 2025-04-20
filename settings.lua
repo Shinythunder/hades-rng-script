7e1914d1be2920d4892d1eaa24a3fd34cc1426d8
Shinythunder <130332657+Shinythunder@users.noreply.github.com>
2025-04-20 01:58:45 -0400

    Update settings.lua

--- settings.lua
+++ settings.lua
@@ -7,4 +7,40 @@ return function(Rayfield, Window)
             Rayfield:Destroy()
         end,
     })
+
+    tab:CreateDropdown({
+        Name = "Themes",
+        Options = {
+            "Default",
+            "Amber Glow",
+            "Amethyst",
+            "Bloom",
+            "Dark Blue",
+            "Green",
+            "Light",
+            "Ocean",
+            "Serenity"
+        },
+        CurrentOption = {"Default"},
+        MultipleOptions = false,
+        Flag = "ThemeSelector",
+        Callback = function(option)
+            local themeMap = {
+                ["Default"] = "Default",
+                ["Amber Glow"] = "AmberGlow",
+                ["Amethyst"] = "Amethyst",
+                ["Bloom"] = "Bloom",
+                ["Dark Blue"] = "DarkBlue",
+                ["Green"] = "Green",
+                ["Light"] = "Light",
+                ["Ocean"] = "Ocean",
+                ["Serenity"] = "Serenity",
+            }
+
+            local themeIdentifier = themeMap[option[1]]
+            if themeIdentifier then
+                Window:ModifyTheme(themeIdentifier)
+            end
+        end,
+    })
 end
